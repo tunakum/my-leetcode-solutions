48a5bb259fbdd2d08b5e9a086bdb82ff53e4740c
Tunahan Kum <tunalogyy@gmail.com>
2025-10-26 22:12:53 +0300

    palindrome question

---
+++ PalindromeNumber.py
@@ -0,0 +1,33 @@
+# x = 121 True
+# x = -121 false
+# x = 10 false
+# x = 1000021
+
+#class Solution(object):
+#      def isPalindrome(self, x):
+#       """
+#       :type x: int
+#       :rtype: bool
+#       """
+
+#x = input("Enter a number\n")
+#print(x[0])
+#print(x[len(x) -1 ])
+
+#x = 121021
+#print(len(str(x)))
+#if (x[i] == x[(len(x) -1) - i]):
+
+
+
+def isPalindrome(x):
+    if (x < 0):
+        return False
+    
+    x = str(x)
+    
+    for i in range(len(x)):
+        if x[i] != x[len(x) - 1 - i]:
+            return False
+        else:
+            return True
