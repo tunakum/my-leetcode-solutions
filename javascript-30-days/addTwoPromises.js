17b1203f81cc3538189a2798ceaa6c6b92df5b01
Tunahan Kum <tunalogyy@gmail.com>
2025-11-06 20:54:26 +0300

    solved add two promises problem

--- javascript-30-days/addTwoPromises.js
+++ javascript-30-days/addTwoPromises.js
@@ -4,7 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    return promise1 + promise2;
+    const value1 = await promise1;
+    const value2 = await promise2;
+    return value1 + value2; 
     };
 
 
